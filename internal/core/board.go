1c84266162772fea9fcbb2db341422a1dd819e3b
jon <jon@ideograph.com>
2025-10-14 08:47:57 -0500

    refactor: useful Cell object initialized with frame and rotation

--- internal/core/board.go
+++ internal/core/board.go
@@ -9,15 +9,17 @@ import (
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
-// Cell is a square on the board.
+// Cell is a physical square on the board.
 type Cell struct {
-	X, Y int
+	frame    animation.FrameLoc
+	rotation rl.Vector2
 }
 
 // Board is the game board, which is a grid of cells.
 type Board struct {
 	Rows, Columns     int
 	backgroundSprites *animation.SpriteSheet
+	cells             [][]Cell
 }
 
 const (
@@ -32,27 +34,43 @@ var (
 
 // newBoard creates a new, empty board.
 func newBoard() Board {
-	return Board{
+	b := Board{
 		Rows:              10,
 		Columns:           10,
+		cells:             make([][]Cell, 10),
 		backgroundSprites: animation.LoadSpriteSheet("dungeon_tiles.png", 4, 9),
 	}
+	for i := range b.cells {
+		b.cells[i] = make([]Cell, 10)
+	}
+
+	// Initialize the board's cells with frames
+	for i := range b.Rows {
+		for j := range b.Columns {
+			var f = WhiteSquare
+			if (i+j)%2 == 0 {
+				f = BlackSquare
+			}
+			b.cells[i][j] = Cell{
+				frame:    f,
+				rotation: rl.Vector2{X: 1, Y: 0},
+			}
+		}
+	}
+
+	return b
 }
 
 // Render draws the board to the screen at the given location.
 func (b *Board) Render(loc rl.Vector2) error {
 	// First draw the board itself
-	for i := 0; i < b.Rows; i++ {
-		for j := 0; j < b.Columns; j++ {
-			var frame = WhiteSquare
-			if (i+j)%2 == 0 {
-				frame = BlackSquare
-			}
+	for i := range b.Rows {
+		for j := range b.Columns {
 			if err := b.backgroundSprites.DrawFrame(
-				frame,
+				b.cells[i][j].frame,
 				rl.Vector2{X: loc.X + float32(j*CellSize*Scale), Y: loc.Y + float32(i*CellSize*Scale)},
 				Scale,
-				rl.Vector2{X: 1, Y: 0}); err != nil {
+				b.cells[i][j].rotation); err != nil {
 				return fmt.Errorf("failed to draw cell: %w", err)
 			}
 		}
